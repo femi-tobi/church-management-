ca7d213d99feca9826dd2aaa98345e4c43f96622
unknown <adefemioluwatobi13@gmail.com>
2025-08-02 11:52:29 +0100

    new

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const {
   commitTransaction 
 } = require('./config');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Instrument list and quantities
 const instrumentList = [
